0b538b77b505dc81380b967183756f03b712c607
Qingyuan Ma <qingyuan@Justins-MacBook-Pro-166.local>
2020-09-20 16:12:13 -0400

    setup the schema planning

--- realestate/settings.py
+++ realestate/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'realestate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'realestate',
+        'USER': 'postgres',
+        'PASSWORD': 'zhang199711',
+        'HOST': 'localhost'
     }
 }
 
