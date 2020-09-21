49dc702df30750f423047465f6a73089c90a81d3
Qingyuan Ma <qingyuan@Justins-MacBook-Pro-166.local>
2020-09-20 21:10:00 -0400

    create superuser and register models with admin

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Listing
+# Register your models here.
+# this is where we customize the admin for the listing app
+
+admin.site.register(Listing)
--- realtors/admin.py
+++ realtors/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Realtor
 # Register your models here.
+# this is where we customize the admin for the listing app
+
+admin.site.register(Realtor)
