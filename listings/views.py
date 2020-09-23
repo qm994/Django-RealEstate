21af7209321ea141847399c0143a5fb2a305823e
Qingyuan Ma <qingyuan@Justins-MacBook-Pro-166.local>
2020-09-23 15:23:23 -0400

    replace hardcoded data

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 from .models import Listing
+from django.core import serializers
 
 # Create your views here.
 #Django uses request and response objects to pass state through the system.
@@ -8,6 +9,7 @@ from .models import Listing
 # The `template_name` will access the `templates` folder and this is setup in the coreapp settings.py file
 def index(request):
     listings = Listing.objects.all()
+    print(listings.values())
     context = {
         'listings': listings
     }
