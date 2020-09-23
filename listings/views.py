005414c224a25363a437c499200baca017d5021c
Qingyuan Ma <qingyuan@Justins-MacBook-Pro-166.local>
2020-09-23 13:26:36 -0400

    load Listings data from db

--- listings/views.py
+++ listings/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
+from .models import Listing
+
 # Create your views here.
 #Django uses request and response objects to pass state through the system.
 
 # The `template_name` will access the `templates` folder and this is setup in the coreapp settings.py file
 def index(request):
-    return render(request, template_name='listings/listings.html')
+    listings = Listing.objects.all()
+    context = {
+        'listings': listings
+    }
+    return render(request, template_name='listings/listings.html', context=context)
 
 def listing(request):
     return render(request, template_name='listings/listing.html')
