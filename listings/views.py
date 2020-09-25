99126db692eb26d762d4730e85a4651f6ab94049
Qingyuan Ma <qingyuan@Justins-MacBook-Pro-166.local>
2020-09-25 12:36:37 -0400

    add pagination

--- listings/views.py
+++ listings/views.py
@@ -2,20 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 from .models import Listing
 from django.core import serializers
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 # Create your views here.
 #Django uses request and response objects to pass state through the system.
 
 # The `template_name` will access the `templates` folder and this is setup in the coreapp settings.py file
 def index(request):
-    listings = Listing.objects.all()
+    # only show the published one
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
 
-    # paginator = Paginator(listings, per_page=3)
-    # page = request.GET.get('page')
-    # page_listings = paginator.get_page(page)
-    # print(paginator)
+    paginator = Paginator(listings, per_page=6)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+    print(paged_listings)
+    
     context = {
-        'listings': listings
+        'listings': paged_listings
     }
     return render(request, template_name='listings/listings.html', context=context)
 
