57c41eb25b7459c2b90ccce75ee134c8d8fe17c9
Qingyuan Ma <qingyuan@Justins-MacBook-Pro-166.local>
2020-09-25 12:51:29 -0400

    use the database data in pages index.html

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
+
+from listings.models import Listing
+
 # Create your views here.
 #Django uses request and response objects to pass state through the system.
 def index(request):
-    return render(request, template_name='pages/index.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+    context = {
+        'listings': listings
+    }
+    return render(request, template_name='pages/index.html', context=context)
 
 def about(request):
     return render(request, template_name='pages/about.html')
