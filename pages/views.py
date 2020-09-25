9b303ec2466a10c7149f69a8c7cd5d00f24f406c
Qingyuan Ma <qingyuan@Justins-MacBook-Pro-166.local>
2020-09-25 13:41:24 -0400

    use the database data in about page

--- pages/views.py
+++ pages/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 
 from listings.models import Listing
-
+from realtors.models import Realtor
 # Create your views here.
 #Django uses request and response objects to pass state through the system.
 def index(request):
@@ -13,4 +13,12 @@ def index(request):
     return render(request, template_name='pages/index.html', context=context)
 
 def about(request):
-    return render(request, template_name='pages/about.html')
+    realtors = Realtor.objects.order_by('-hire_date')
+    # get mvp
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+    context = {
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors
+    }
+
+    return render(request, template_name='pages/about.html', context=context)
