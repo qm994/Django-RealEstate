1c3ca710a14d6218c7f7929c0ba99648b3aec1d8
Qingyuan Ma <qingyuan@Justins-MacBook-Pro-166.local>
2020-09-26 14:16:21 -0400

    search form filtering

--- listings/views.py
+++ listings/views.py
@@ -30,7 +30,36 @@ def listing(request, listing_id):
     return render(request, template_name='listings/listing.html', context=context)
 
 def search(request):
+    queryset_list = Listing.objects.order_by('-list_date')
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(description__icontains=keywords)
+    
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            queryset_list = queryset_list.filter(city__iexact=city)
+    
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            queryset_list = queryset_list.filter(state__iexact=state)
+    
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            queryset_list = queryset_list.filter(bedrooms__lte=bedrooms)
+    
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            queryset_list = queryset_list.filter(price__lte=price)
+    
+    
+    print(queryset_list)
     context = {
+        'listings': queryset_list,
         'state_choices': state_choices,
         'price_choices': price_choices,
         'bedroom_choices': bedroom_choices
