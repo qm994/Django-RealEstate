753e2cfdcedcd3d9cc61e69cafdfef201feec4ce
Qingyuan Ma <qingyuan@Justins-MacBook-Pro-166.local>
2020-10-25 14:21:31 -0400

    finish the login and register html

--- accounts/views.py
+++ accounts/views.py
@@ -3,19 +3,24 @@ from django.http import HttpResponse, HttpRequest
 # Create your views here.
 
 def login(request):
-    
-    return render(request, template_name='accounts/login.html')
+    if request.method == 'POST':
+        print('Submit the form!')
+        # login the user
+        pass
+    else:
+        return render(request, template_name='accounts/login.html')
 
 def register(request):
-    
-    return render(request, template_name='accounts/register.html')
+    if request.method == 'POST':
+        print('Submit the form!')
+        # register the user
+        pass
+    else:
+        return render(request, template_name='accounts/register.html')
 
 def dashboard(request):
     
     return render(request, template_name='accounts/dashboard.html')
-
-def index(request):
-    pass
 
 def logout(request):
     # redirect to the index view, and the view will be retrieved by reverse()
