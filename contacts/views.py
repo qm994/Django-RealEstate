5b91e931519f8340e277382868da736d81eacf4e
Qingyuan Ma <qingyuan@Justins-MacBook-Pro-166.local>
2020-10-29 16:38:03 -0400

    finish contact form submission

--- contacts/views.py
+++ contacts/views.py
@@ -1,4 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Contacts
+from django.contrib import messages
+
 
 def contact(request):
-    pass
+    if request.method == 'POST':
+        listing_id = request.POST['listing_id']
+        listing = request.POST['listing']
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        message = request.POST['message']
+        user_id = request.POST['user_id']
+        realtor_email = request.POST['realtor_email']
+
+        contacts = Contacts(listing=listing, listing_id=listing_id, name=name,
+            email=email, phone=phone, message=message, user_id=user_id)
+        contacts.save()
+
+        messages.success(request, 'You request has been submitted! I will get back to u soon!')
+
+    return redirect('/listings/' + listing_id)
+
