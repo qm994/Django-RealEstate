0ef37cf42e034c03e2f6ffc5eb2474a61ed0862f
Qingyuan Ma <qingyuan@Justins-MacBook-Pro-166.local>
2020-10-30 17:25:55 -0400

    a few bugs fix

--- realestate/urls.py
+++ realestate/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('', include('pages.urls')),
     path('listings/', include('listings.urls')),
@@ -11,5 +12,3 @@ urlpatterns = [
     path('contacts/', include('contacts.urls')),
     path('admin/', admin.site.urls)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
