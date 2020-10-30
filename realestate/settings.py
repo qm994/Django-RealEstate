7c4dc5ddb9d754bb3d3c5273ab2a0e8c5bf8a2b1
Qingyuan Ma <qingyuan@Justins-MacBook-Pro-166.local>
2020-10-30 14:10:25 -0400

    rm email config and add it local settings

--- realestate/settings.py
+++ realestate/settings.py
@@ -150,12 +150,12 @@ MESSAGE_TAGS = {
 
 # EMAIL CONFIG
 # 'localhost' -> test on local 
-EMAIL_HOST = 'smtp.gamil.com'
-# 25 as default
-EMAIL_PORT = 587
-EMAIL_HOST_USER = str(os.getenv('EMAIL_HOST_USER'))
-EMAIL_HOST_PASSWORD = str(os.getenv('EMAIL_HOST_PASSWORD'))
-EMAIL_USE_TLS = True
+# EMAIL_HOST = 'smtp.gamil.com'
+# # 25 as default
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = str(os.getenv('EMAIL_HOST_USER'))
+# EMAIL_HOST_PASSWORD = str(os.getenv('EMAIL_HOST_PASSWORD'))
+# EMAIL_USE_TLS = True
 
 # for the production server
 try:
