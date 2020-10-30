90690df76860ee60361438ef4ab3024ce3c29e2a
Qingyuan Ma <qingyuan@Justins-MacBook-Pro-166.local>
2020-10-30 13:48:19 -0400

    add prod settings

--- realestate/settings.py
+++ realestate/settings.py
@@ -156,3 +156,9 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = str(os.getenv('EMAIL_HOST_USER'))
 EMAIL_HOST_PASSWORD = str(os.getenv('EMAIL_HOST_PASSWORD'))
 EMAIL_USE_TLS = True
+
+# for the production server
+try:
+    from .local_settings import *
+except ImportError:
+    pass
