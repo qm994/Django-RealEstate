b30526a0b64f4fa292f53b8e55f64b6fccc6664f
Qingyuan Ma <qingyuan@Justins-MacBook-Pro-166.local>
2020-11-03 20:41:17 -0500

    debug to false

--- realestate/settings.py
+++ realestate/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('DJANGO_SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
 
